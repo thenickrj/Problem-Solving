875ab41f7e602b32c84574af01d0ff585b464442
Nikhil Singh <nikhilbangalor@gmail.com>
2021-04-02 09:59:19 +0530

    Update Container with the Most Water.py

--- Container with the Most Water.py
+++ Container with the Most Water.py
@@ -17,7 +17,7 @@ def maxArea(h):
     l=len(h)-1
     m=0 
     while(f!=l): #as long as the first and last doesn't collide.
-    #Traverse the whole array to find the area with the maximum space that can contain the water
+    #Traverse the whole list to find the area with the maximum space that can contain the water
         print(min(h[f],h[l])*(l-f))
         if((min(h[f],h[l])*(l-f))>m):
             m=min(h[f],h[l])*(l-f)
