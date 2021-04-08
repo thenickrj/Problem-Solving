ab6e75e6c30f6eddd0d462257e4bb9b4408196ca
Nikhil Singh <nikhilbangalor@gmail.com>
2021-04-08 09:50:40 +0530

    Update Reverse String.py

--- Reverse String.py
+++ Reverse String.py
@@ -1,3 +1,17 @@
+# Reverse String
+
+"""
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+Example 1:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+"""
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
