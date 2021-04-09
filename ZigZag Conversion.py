f48042a3f2390eae6b6ef64f971c089fad0d0a22
Nikhil Singh <nikhilbangalor@gmail.com>
2021-04-09 09:31:33 +0530

    Add files via upload

---
+++ ZigZag Conversion.py
@@ -0,0 +1,58 @@
+# ZigZag Conversion
+
+
+"""
+
+The string "PAYPALISHIRING" is written in a zigzag pattern on a 
+given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+    P   A   H   N
+    A P L S I I G
+    Y   I   R
+
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+ 
+
+Example 1:
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+
+Example 2:
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+
+Example 3:
+Input: s = "A", numRows = 1
+Output: "A"
+
+"""
+
+def convert(s,numRows):
+    if numRows == 1:
+        return s
+        
+    row = 0
+    mode = -1
+    ans = ['']*numRows
+    print(ans)    
+    for i in s:
+        ans[row] += i
+        if row == 0 or row == (numRows-1):
+            mode *= -1
+        print(ans[row])    
+        row += mode
+            
+    return ''.join(ans)
+
+
+x=convert("PAYPALISHIRING",3)        
+print(x)
